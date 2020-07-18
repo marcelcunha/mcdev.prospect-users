04588119b2e3bcbb5b4bccc153fc4ed9a88328bb
Marcel Cunha <marcel.cunha.mc@gmail.com>
2020-07-18 13:22:18 -0300

    ajustes no namespace de algumas classes

--- src/Http/Controllers/ProspectUserController.php
+++ src/Http/Controllers/ProspectUserController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ProspectUser;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use MCDev\ProspectUsers\Models\ProspectUser;
 use MCDev\ProspectUsers\Http\Requests\ProspectRequest;
 
-class CandidateUserController extends Controller
+class ProspectUserController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -27,7 +27,7 @@ class CandidateUserController extends Controller
      */
     public function create()
     {
-        return view('');
+        return view('prospect-users::register');
     }
 
     /**
--- src/Observers/ProspectUserObserver.php
+++ src/Observers/ProspectUserObserver.php
@@ -1,13 +1,23 @@
 <?php
 
-namespace MCdev\Observers\Observers;
+namespace MCDev\ProspectUsers\Observers;
 
-use App\Models\ProspectUser;
+use Exception;
+use MCDev\ProspectUsers\Models\ProspectUser;
 
 class ProspectUserObserver
 {
+    /**
+     * Captura o evento de criação de um novo prospect e tenta enviar um email para o mesmo, caso algo dê errado, o prospect é removido e a exceção continua seu caminho.
+     */
     public function created(ProspectUser $prospectUser)
     {
-        $prospectUser->sendEmailNotification();
+        try {
+            $prospectUser->sendEmailNotification();
+        } catch (Exception $e) {
+            $prospectUser->delete();
+
+            throw $e;
+        }
     }
 }
